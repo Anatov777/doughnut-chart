ad2339499dd230ca63f5923d250e1dbf5d10a3b5
Anatov777 <anatovtimur@gmail.com>
2020-11-16 10:13:16 +0300

    add media

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
+    <header class="header">
+      <div class="container">
+        <h1>В настоящий момент в министерстве магии</h1>
+      </div>
+    </header>
     <PieChart />
+    <footer class="footer">
+      <div class="container">
+        <div class="copyright">
+          ООО “Нейрософт”, 2020<br />
+          Медицинское оборудование
+        </div>
+        <div class="socials">
+          <div class="socials__text">Подписывайся в соц сетях</div>
+          <div class="socials__icons">
+            <a href="#"><img src="/images/inst.svg" alt="Instagram" /></a>
+            <a href="#"><img src="/images/vk.svg" alt="VK" /></a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import PieChart from './components/PieChart.vue'
+import PieChart from "./components/PieChart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    PieChart
-  }
-}
+    PieChart,
+  },
+};
 </script>
 
 <style lang="scss">
-  @import "./assets/style.scss";
+@import "./assets/style.scss";
+@import "./assets/media.scss";
 </style>
--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,82 +1,112 @@
 <template>
-  <div class="pie-chart">
-    <canvas ref="canvas" id="departmentChart"></canvas>
-    <div id="legend"></div>
+  <div class="chart-area">
+    <div class="container">
+      <div class="info">
+        <div class="info__item">
+          <div class="info__num">{{ serverData.data.total }}</div>
+          <div class="info__title">{{ empNumsTitle[0] }}</div>
+        </div>
+        <div class="info__item">
+          <div class="info__num">{{ serverData.data.males }}</div>
+          <div class="info__title">{{ empNumsTitle[1] }}</div>
+        </div>
+        <div class="info__item">
+          <div class="info__num">{{ serverData.data.females }}</div>
+          <div class="info__title">{{ empNumsTitle[2] }}</div>
+        </div>
+        <div class="info__item">
+          <div class="info__num">{{ serverData.data.couples }}</div>
+          <div class="info__title">{{ empNumsTitle[3] }}</div>
+        </div>
+      </div>
+      <h2>По департаментам</h2>
+      <div class="doughnut-chart">
+        <canvas
+          ref="canvas"
+          id="departmentChart"
+          width="100%"
+          height="100%"
+        ></canvas>
+        <div class="legend" id="legend"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { Doughnut } from "vue-chartjs";
-import "chartjs-plugin-labels";
+import "chartjs-plugin-datalabels";
 
 export default {
   name: "PieChart",
   extends: Doughnut,
-  // props: {
-  //   msg: String
-  // }
-  // data: () => ({
-  //   htmlLegend: null
-  // }),
+  data: () => ({
+    serverData: require("@/test.json"),
+    empNumsTitle: ["Сотрудников в штате", "Мужчины", "Женщин", "Пар"],
+  }),
   mounted() {
+    let self = this;
     const data = {
       datasets: [
         {
-          data: [3, 54, 13, 22],
-          backgroundColor: ["#2B78FE", "#8295FA", "#FFBB49", "#87D227"],
+          data: this.serverData.data.departments.map((emp) => emp.employees),
+          backgroundColor: [
+            "#2B78FE",
+            "#8295FA",
+            "#FFBB49",
+            "#87D227",
+            "#20CBAC",
+            "#01B0D7",
+            "#6235B0",
+            "#9559FF",
+            "#C055D1",
+            "#FF7979",
+          ],
         },
       ],
-
-      labels: [
-        "Администрация",
-        "Отдел обеспечения магического правопорядка",
-        "Отдел магических происшествий и катастроф",
-        "Отдел регулирования магических популяций",
-      ],
+      labels: this.serverData.data.departments.map((name) => name.title),
     };
     const options = {
-      plugins: {
-        labels: {
-          render: "percentage",
-          fontSize: 30,
-          fontFamily: "Helvetica Neue",
-          fontStyle: "normal",
-          fontColor: "#fff",
-          precision: 1,
-          overlap: false,
+      cutoutPercentage: 56,
+      legend: false,
+      elements: {
+        arc: {
+          borderWidth: 0,
         },
       },
-      legend: false,
-      legendCallback: function (chart) {
-        var list = document.createElement("ul");
-        var data = chart.data;
-        var datasets = data.datasets;
-        var labels = data.labels;
-        var i, ilen, listItem, listItemSpan;
-        list.setAttribute("class", chart.id + "-legend");
-        if (datasets.length) {
-          for (i = 0, ilen = datasets[0].data.length; i < ilen; ++i) {
-            listItem = list.appendChild(document.createElement("li"));
-            listItemSpan = listItem.appendChild(document.createElement("span"));
-            listItemSpan.style.backgroundColor = datasets[0].backgroundColor[i];
-            if (labels[i]) {
-              listItem.appendChild(document.createTextNode(labels[i]));
+      plugins: {
+        datalabels: {
+          display: "auto",
+          color: "#fff",
+          font: function (context) {
+            console.log(context);
+            var width = context.chart.width;
+            var size = Math.round(width / 20);
+            return {
+              size: size,
+              family: "Roboto",
+              weight: 500
+            };
+          },
+          formatter: function (value, context) {
+            if (+((value * 100) / self.total(context)).toFixed(1) > 5) {
+              return ((value * 100) / self.total(context)).toFixed(1) + "%";
+            } else {
+              return "";
             }
-          }
-        }
-
-        return list.outerHTML;
+          },
+        },
       },
     };
 
     this.renderChart(data, options);
+
     let legendContainer = document.getElementById("legend");
     legendContainer.innerHTML = this.generateLegend();
     var legendItems = legendContainer.getElementsByTagName("li");
     for (var i = 0; i < legendItems.length; i += 1) {
       legendItems[i].addEventListener("click", this.legendClickCallback, false);
     }
-    // console.log(this.$data._chart);
   },
   methods: {
     legendClickCallback(event) {
@@ -101,33 +131,16 @@ export default {
       }
       chart.update();
     },
+    total(chart) {
+      let data = chart.chart.data.datasets[0].data;
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      var total = data.reduce(reducer);
+      return total;
+    },
   },
 };
 </script>
 
 <style lang="scss">
 @import "../assets/chart.scss";
-
-[class$="-legend"] {
-  list-style: none;
-  cursor: pointer;
-  padding-left: 0;
-}
-
-[class$="-legend"] li {
-  display: inline-block;
-  padding: 0 5px;
-}
-
-[class$="-legend"] li.hidden {
-  text-decoration: line-through;
-}
-
-[class$="-legend"] li span {
-  border-radius: 5px;
-  display: inline-block;
-  height: 10px;
-  margin-right: 10px;
-  width: 10px;
-}
 </style>
