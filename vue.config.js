2f4ea09ab8cc6fa341a2c046725a62be0496f6ae
Anatov777 <anatovtimur@gmail.com>
2021-06-21 20:22:33 +0300

    change hand image path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/doughnut-chart/' : '/',
-  assetsDir: process.env.NODE_ENV === 'production' ? '/doughnut-chart/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/doughnut-chart/' : '/'
 }
